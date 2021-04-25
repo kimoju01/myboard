82d339e8989051bb9a9acc76d4807d3485008c8c
kimoju01 <kimoju03@naver.com>
2021-04-25 15:57:05 +0900

    test: 게시글 저장 확인 테스트

---
+++ src/test/java/com/hyeju/study/myboard/domain/repository/BoardRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.hyeju.study.myboard.domain.repository;
+
+import com.hyeju.study.myboard.domain.entity.BoardEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class BoardRepositoryTest {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @AfterEach
+    public void cleanup() {
+        boardRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글 제목";
+        String content = "테스트 게시글 내용";
+
+        boardRepository.save(BoardEntity.builder()
+                .title(title)
+                .content(content)
+                .writer("kim")
+                .build());
+        //when
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        //then
+        BoardEntity boardEntity = boardEntityList.get(0);
+        Assertions.assertThat(boardEntity.getTitle()).isEqualTo(title);
+        Assertions.assertThat(boardEntity.getContent()).isEqualTo(content);
+
+    }
+
+}
