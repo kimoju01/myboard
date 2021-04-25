eb8cd360868fa460a677b22396373b224755893b
kimoju01 <kimoju03@naver.com>
2021-04-25 20:52:54 +0900

    style: 코드 정렬 변경

--- src/main/java/com/hyeju/study/myboard/domain/entity/BoardEntity.java
+++ src/main/java/com/hyeju/study/myboard/domain/entity/BoardEntity.java
@@ -16,21 +16,20 @@ public class BoardEntity extends TimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 10, nullable = false)
-    private String writer;
-
     @Column(length = 100, nullable = false)
     private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    @Column(length = 10, nullable = false)
+    private String writer;
+
     @Builder
-    public BoardEntity(Long id, String writer, String title, String content) {
-        this.id = id;
-        this.writer = writer;
+    public BoardEntity(String title, String content, String writer) {
         this.title = title;
         this.content = content;
+        this.writer = writer;
     }
 
 }
