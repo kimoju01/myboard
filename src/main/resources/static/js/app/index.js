791276cd895d92bcd039dbf197dbc3c943b08901
kimoju01 <kimoju03@naver.com>
2021-05-13 19:50:41 +0900

    feat: 회원가입 위한 ajax

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -12,6 +12,14 @@ var main = {
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
+
+        $('#btn-member-join').on('click', function () {
+            _this.join();
+        });
+
+        // $('#btn-member-login').on('click', function () {
+        //     _this.login();
+        // });
     },
 
     save: function () {
@@ -79,7 +87,65 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+
+    join: function () {
+        var userdata = {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            password: $('#password').val()
+        };
+
+        if (userdata.name == "" || userdata.email == "" || userdata.password == "") {
+            alert("정보를 모두 입력해주세요.");
+            return;
+        }
+        ;
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/members/new',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(userdata)
+        }).done(function () {
+            alert("회원 가입이 완료되었습니다.");
+            window.location.href = '/login';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
+
+    // },
+
+    // login: function () {
+    //     var userdata = {
+    //         email: $('#email').val(),
+    //         password: $('#password').val()
+    //     };
+    //
+    //     if (userdata.email == "") {
+    //         alert("이메일을 입력해주세요.");
+    //         return;
+    //     } else if (userdata.password == "") {
+    //         alert("비밀번호를 입력해주세요.");
+    //         return;
+    //     };
+    //
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: '/api/v1/members/login',
+    //         dataType: 'json',
+    //         contentType: 'application/json; charset=utf-8',
+    //         data: JSON.stringify(userdata)
+    //     }).done(function () {
+    //         alert(userdata.email + "님 반갑습니다!");
+    //         window.location.href = '/';
+    //     }).fail(function (error) {
+    //         alert(JSON.stringify(error));
+    //     });
+    // }
+
 };
 
 main.init();
