8c624f830f7d0cf330956262251db10f06c7b023
kimoju01 <kimoju03@naver.com>
2021-05-18 20:50:02 +0900

    feat: 스프링 시큐리티 설정 추가
    default parameter 변경과 OAuth는 잠시 비활성화

--- src/main/java/com/hyeju/study/myboard/config/auth/WebSecurityConfig.java
+++ src/main/java/com/hyeju/study/myboard/config/auth/WebSecurityConfig.java
@@ -1,20 +1,28 @@
 package com.hyeju.study.myboard.config.auth;
 
-import com.hyeju.study.myboard.domain.user.Role;
+import com.hyeju.study.myboard.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private final CustomOAuth2UserService customOAuth2UserService;
+    private final MemberService memberService;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/scss/**", "/images/**", "/js/**", "/h2-console/**");
+        web.ignoring().antMatchers("/css/**", "/scss/**", "/images/**", "/js/**", "/fonts/**", "**/favicon.ico", "/h2-console/**");
     }
 
     @Override
@@ -22,22 +30,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/", "/login", "/about").permitAll()
-//                    .antMatchers("/api/v1/**", "/posts/**").hasRole("USER")
+                    .antMatchers("/", "/about",  "/loginPage", "/joinPage", "/api/v1/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/login")
+                    .loginPage("/loginPage")
+                    .defaultSuccessUrl("/")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
                     .permitAll()
                     .and()
                 .logout()
                     .logoutSuccessUrl("/")
-                    .permitAll()
-                    .and()
-                .oauth2Login()
-                    .and()
-                .exceptionHandling()
-                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+                    .permitAll();
+//                    .and()
+//                .oauth2Login()
+//                    .loginPage("/members/login")
+//                    .userInfoEndpoint()
+//                    .userService(customOAuth2UserService);
+//                    .and()
+//                .exceptionHandling()
+//                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
 
     }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(memberService)
+//                .passwordEncoder(new BCryptPasswordEncoder());
+//    }
 }
