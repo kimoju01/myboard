b21ba149ef17d7df5bdd16bbbb8d5e0b58203e2d
kimoju01 <kimoju03@naver.com>
2021-05-19 18:52:10 +0900

    feat: 스프링 시큐리티 UserDetailsService 구현

---
+++ src/main/java/com/hyeju/study/myboard/config/auth/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.hyeju.study.myboard.config.auth;
+
+import com.hyeju.study.myboard.domain.member.entity.MemberEntity;
+import com.hyeju.study.myboard.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    //스프링이 로그인 요청을 가로챌 때, username과 password를 가로채는데 username이 db에 있는지만 확인해주면 됨. 난 username => email이니 email로 찾기
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberEntity memberEntity = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다."));
+        return new CustonUserDetails(memberEntity); //시큐리티의 세션에 유저정보가 저장됨
+    }
+}
