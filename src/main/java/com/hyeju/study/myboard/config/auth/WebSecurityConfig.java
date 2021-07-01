82981bffe0244a7dbdd142341a56e55d04860a6e
kimoju01 <kimoju03@naver.com>
2021-07-01 23:12:17 +0900

    feat: 카카오 회원가입 가능하도록 구현
    - 근데 아직 로그인 처리가 안 된다
    
    관련: #5

--- src/main/java/com/hyeju/study/myboard/config/auth/WebSecurityConfig.java
+++ src/main/java/com/hyeju/study/myboard/config/auth/WebSecurityConfig.java
@@ -3,21 +3,29 @@ package com.hyeju.study.myboard.config.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RequiredArgsConstructor
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration  //빈 등록 (객체 생성)
+@EnableWebSecurity  //필터 체인에 등록 (스프링 시큐리티 활성화)
+@EnableGlobalMethodSecurity(prePostEnabled = true)  //특정 주소 접근 시 권한 및 인증을 미리 체크한다.
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -25,6 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     //시큐리티가 가로채서 로그인할 때, 해당 비밀번호가 어떤 해쉬로 암호화 되었는지 알아야 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+    /* userDetailsServie 방식 */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService)
@@ -42,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/", "/about",  "/login", "/register", "/api/v1/**").permitAll()
+                    .antMatchers("/", "/about",  "/login", "/register", "/api/v1/**", "/oauth/kakao/callback").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
@@ -53,12 +62,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .passwordParameter("password")
                     .permitAll()
                     .and()
+//                .oauth2Login()
+//                    .defaultSuccessUrl("/", true)
+//                    .and()
                 .logout()
                     .logoutSuccessUrl("/")
                     .permitAll();
-//                    .and()
+//                .and()
 //                .oauth2Login()
-//                    .loginPage("/members/login")
+//                .loginPage("/members/login");
 //                    .userInfoEndpoint()
 //                    .userService(customOAuth2UserService);
 //                    .and()
