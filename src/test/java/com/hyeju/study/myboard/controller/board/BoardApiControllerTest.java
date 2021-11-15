533cd142069ea9195ed60ea5f8062025defc3f45
kimoju01 <kimoju03@naver.com>
2021-11-15 18:04:56 +0900

    fix

--- src/test/java/com/hyeju/study/myboard/controller/board/BoardApiControllerTest.java
+++ src/test/java/com/hyeju/study/myboard/controller/board/BoardApiControllerTest.java
@@ -1,103 +1,103 @@
-package com.hyeju.study.myboard.controller.board;
-
-import com.hyeju.study.myboard.domain.board.entity.BoardEntity;
-import com.hyeju.study.myboard.domain.board.repository.BoardRepository;
-import com.hyeju.study.myboard.dto.BoardSaveRequestDto;
-import com.hyeju.study.myboard.dto.BoardUpdateRequestDto;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.List;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class BoardApiControllerTest {
-
-    @LocalServerPort
-    public int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private BoardRepository boardRepository;
-
-    @AfterEach
-    public void cleanup() throws Exception {
-        boardRepository.deleteAll();
-    }
-
-    @Test
-    public void 게시글_등록() throws Exception {
-        //given
-        String title = "title";
-        String content = "content";
-
-        BoardSaveRequestDto requestDto = BoardSaveRequestDto.builder()
-                .title(title)
-                .content(content)
-                .build();
-
-        String url = "http://localhost:" + port + "/api/v1/post";
-
-        //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-        /* postForEntity : post 요청을 보내고 그 결과(json 값)를 ResponseEntity로 반환받는다. */
-
-        //then
-        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
-
-        List<BoardEntity> all = boardRepository.findAll();
-        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
-        Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
-
-    }
-
-    @Test
-    public void 게시글_수정() throws Exception {
-        //given
-        BoardEntity savedPost = boardRepository.save(BoardEntity.builder()
-                .title("title")
-                .content("content")
-                .build());
-
-        Long updateId = savedPost.getId();
-        String expectedTitle = "title2";
-        String expectedContent = "content2";
-
-        BoardUpdateRequestDto requestDto = BoardUpdateRequestDto.builder()
-                .title(expectedTitle)
-                .content(expectedContent)
-                .build();
-
-        String url = "http://localhost:" + port + "/api/v1/post/" + updateId;
-
-        HttpEntity<BoardUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-
-        //when
-        ResponseEntity<Long> responseEntity = restTemplate
-                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
-
-        //then
-        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
-        List<BoardEntity> all = boardRepository.findAll();
-        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-        Assertions.assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-    }
-
-}
+//package com.hyeju.study.myboard.controller.board;
+//
+//import com.hyeju.study.myboard.domain.board.entity.BoardEntity;
+//import com.hyeju.study.myboard.domain.board.repository.BoardRepository;
+//import com.hyeju.study.myboard.dto.BoardSaveRequestDto;
+//import com.hyeju.study.myboard.dto.BoardUpdateRequestDto;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.web.server.LocalServerPort;
+//import org.springframework.http.HttpEntity;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import java.util.List;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//class BoardApiControllerTest {
+//
+//    @LocalServerPort
+//    public int port;
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Autowired
+//    private BoardRepository boardRepository;
+//
+//    @AfterEach
+//    public void cleanup() throws Exception {
+//        boardRepository.deleteAll();
+//    }
+//
+//    @Test
+//    public void 게시글_등록() throws Exception {
+//        //given
+//        String title = "title";
+//        String content = "content";
+//
+//        BoardSaveRequestDto requestDto = BoardSaveRequestDto.builder()
+//                .title(title)
+//                .content(content)
+//                .build();
+//
+//        String url = "http://localhost:" + port + "/api/v1/post";
+//
+//        //when
+//        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+//        /* postForEntity : post 요청을 보내고 그 결과(json 값)를 ResponseEntity로 반환받는다. */
+//
+//        //then
+//        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//
+//
+//        List<BoardEntity> all = boardRepository.findAll();
+//        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
+//        Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
+//
+//    }
+//
+//    @Test
+//    public void 게시글_수정() throws Exception {
+//        //given
+//        BoardEntity savedPost = boardRepository.save(BoardEntity.builder()
+//                .title("title")
+//                .content("content")
+//                .build());
+//
+//        Long updateId = savedPost.getId();
+//        String expectedTitle = "title2";
+//        String expectedContent = "content2";
+//
+//        BoardUpdateRequestDto requestDto = BoardUpdateRequestDto.builder()
+//                .title(expectedTitle)
+//                .content(expectedContent)
+//                .build();
+//
+//        String url = "http://localhost:" + port + "/api/v1/post/" + updateId;
+//
+//        HttpEntity<BoardUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+//
+//        //when
+//        ResponseEntity<Long> responseEntity = restTemplate
+//                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+//
+//        //then
+//        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//
+//        List<BoardEntity> all = boardRepository.findAll();
+//        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+//        Assertions.assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+//    }
+//
+//}
