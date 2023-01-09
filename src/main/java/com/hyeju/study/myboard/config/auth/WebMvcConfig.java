58242e6d9a0022cb6d97c6c063c15efbd02d73fc
Hyeju <kimoju03@naver.com>
2023-01-09 20:50:16 +0900

    feat: 외부 경로 리소스를 주소로 불러올 수 있도록 수정
    관련: #42, #54

--- src/main/java/com/hyeju/study/myboard/config/auth/WebMvcConfig.java
+++ src/main/java/com/hyeju/study/myboard/config/auth/WebMvcConfig.java
@@ -7,10 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    // 외부 경로 리소스 접근
+    // 외부 경로 리소스를 url로 바로 불러올 수 있도록 설정
+    // localhost:8080/summernoteImage/1111.jpg 로 접속하면 .../Summernote_image/1111.jpa 파일 불러온다.
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/summernoteImage/**")
-                .addResourceLocations("file:///C:/summernote_image/");
+                .addResourceLocations("file:///C:/Study/Project_documents/myboard_documents/Summernote_image/");
+        registry.addResourceHandler("/thumbnailImage/**")
+                .addResourceLocations("file:///C:/Study/Project_documents/myboard_documents/Thumbnail_Image/");
     }
 }
