6320f6a3da5386b078c4c57e3693ef133828f93c
Hyeju <kimoju03@naver.com>
2023-01-17 21:12:38 +0900

    feat test: AWS S3 업로드 테스트

--- src/main/java/com/hyeju/study/myboard/service/board/BoardService.java
+++ src/main/java/com/hyeju/study/myboard/service/board/BoardService.java
@@ -24,56 +24,66 @@ import java.util.stream.Collectors;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final S3Uploader s3Uploader;
 
-    /* 게시글 등록 */
+    /* 게시글 등록 (S3) */
     @Transactional
     public Long save(BoardSaveRequestDto requestDto, MultipartFile multipartFile, MemberEntity memberEntity) throws IOException {
         requestDto.setCount(0L);
         requestDto.setMemberEntity(memberEntity);
 
-        if (multipartFile != null) {    // 왜 isEmpty()는 안 됐지?
-            String fileRoot = "C:\\Study\\Project_documents\\myboard_documents\\Thumbnail_Image\\";
-            String originalFileName = multipartFile.getOriginalFilename();
-            String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
-            String savedFileName = UUID.randomUUID() + extension;
+        if (multipartFile != null) {
+            String uploadImageUrl = s3Uploader.upload(multipartFile, "ThumbnailImage");
 
-            File saveFile = new File(fileRoot, savedFileName);
-            multipartFile.transferTo(saveFile);
-
+            // url => [https://버킷 주소/버킷 폴더 이름/파일명]
+            String savedFileName = uploadImageUrl.substring(uploadImageUrl.lastIndexOf("/") + 1);
             requestDto.setThumbFileName(savedFileName);
-            requestDto.setThumbFilePath(fileRoot + savedFileName);
+            requestDto.setThumbFilePath(uploadImageUrl);
         }
 
         return boardRepository.save(requestDto.toEntity()).getId();
     }
 
-    /* 게시글 수정 */
+    /* 게시글 등록 (로컬) */
+//    @Transactional
+//    public Long save(BoardSaveRequestDto requestDto, MultipartFile multipartFile, MemberEntity memberEntity) throws IOException {
+//        requestDto.setCount(0L);
+//        requestDto.setMemberEntity(memberEntity);
+//
+//        if (multipartFile != null) {    // 왜 isEmpty()는 안 됐지?
+//            String fileRoot = "C:\\Study\\Project_documents\\myboard_documents\\Thumbnail_Image\\";
+//            String originalFileName = multipartFile.getOriginalFilename();
+//            String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+//            String savedFileName = UUID.randomUUID() + extension;
+//
+//            File saveFile = new File(fileRoot, savedFileName);
+//            multipartFile.transferTo(saveFile);
+//
+//            requestDto.setThumbFileName(savedFileName);
+//            requestDto.setThumbFilePath(fileRoot + savedFileName);
+//        }
+//
+//        return boardRepository.save(requestDto.toEntity()).getId();
+//    }
+
+    /* 게시글 수정 (S3) */
     @Transactional
-//    public Long update(Long id, BoardUpdateRequestDto requestDto) {
     public Long update(Long id, BoardUpdateRequestDto requestDto, String oldThumbFileName, MultipartFile multipartFile) throws IOException {
         BoardEntity boardEntity = boardRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다. id=" + id));
 
         if (multipartFile != null && multipartFile.getSize() > 0) {  // 새로운 파일이 전송된 경우 (사이즈가 0보다 큰 경우) => != null 조건 없으면 에러나네
-
-            String fileRoot = "C:\\Study\\Project_documents\\myboard_documents\\Thumbnail_Image\\";
-            String originalFileName = multipartFile.getOriginalFilename();
-            String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
-            String savedFileName = UUID.randomUUID() + extension;
-            
             if (oldThumbFileName != null) {  // 기존에 썸네일 이미지가 존재했던 경우
                 // 기존 존재하던 썸네일 이미지 삭제
-                File oldFile = new File(fileRoot + oldThumbFileName);
-                if (oldFile.exists()) {
-                    oldFile.delete();
-                }
+                s3Uploader.removeObject(oldThumbFileName);
             }
-            
+
             // 새 썸네일 이미지 저장
-            File saveFile = new File(fileRoot, savedFileName);
-            multipartFile.transferTo(saveFile);
+            String uploadImageUrl = s3Uploader.upload(multipartFile, "ThumbnailImage");
+
+            String savedFileName = uploadImageUrl.substring(uploadImageUrl.lastIndexOf("/") + 1);
             requestDto.setThumbFileName(savedFileName);
-            requestDto.setThumbFilePath(fileRoot + savedFileName);
+            requestDto.setThumbFilePath(uploadImageUrl);
 
             boardEntity.updateWithFile(requestDto.getTitle(), requestDto.getContent(), requestDto.getThumbFileName(), requestDto.getThumbFilePath());
         }
@@ -84,22 +94,74 @@ public class BoardService {
         return id;
     }
 
-    /*게시글 삭제 */
+    /* 게시글 수정 (로컬) */
+//    @Transactional
+////    public Long update(Long id, BoardUpdateRequestDto requestDto) {
+//    public Long update(Long id, BoardUpdateRequestDto requestDto, String oldThumbFileName, MultipartFile multipartFile) throws IOException {
+//        BoardEntity boardEntity = boardRepository.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다. id=" + id));
+//
+//        if (multipartFile != null && multipartFile.getSize() > 0) {  // 새로운 파일이 전송된 경우 (사이즈가 0보다 큰 경우) => != null 조건 없으면 에러나네
+//
+//            String fileRoot = "C:\\Study\\Project_documents\\myboard_documents\\Thumbnail_Image\\";
+//            String originalFileName = multipartFile.getOriginalFilename();
+//            String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+//            String savedFileName = UUID.randomUUID() + extension;
+//
+//            if (oldThumbFileName != null) {  // 기존에 썸네일 이미지가 존재했던 경우
+//                // 기존 존재하던 썸네일 이미지 삭제
+//                File oldFile = new File(fileRoot + oldThumbFileName);
+//                if (oldFile.exists()) {
+//                    oldFile.delete();
+//                }
+//            }
+//
+//            // 새 썸네일 이미지 저장
+//            File saveFile = new File(fileRoot, savedFileName);
+//            multipartFile.transferTo(saveFile);
+//            requestDto.setThumbFileName(savedFileName);
+//            requestDto.setThumbFilePath(fileRoot + savedFileName);
+//
+//            boardEntity.updateWithFile(requestDto.getTitle(), requestDto.getContent(), requestDto.getThumbFileName(), requestDto.getThumbFilePath());
+//        }
+//
+//        // 파일이 전송되지 않았을 경우엔 제목과 글만 저장
+//        boardEntity.update(requestDto.getTitle(), requestDto.getContent());
+//
+//        return id;
+//    }
+
+    /* 게시글 삭제 (S3) */
     @Transactional
     public void delete(Long id) {
         BoardEntity boardEntity = boardRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다. id=" + id));
-        
+
         // 썸네일 이미지가 존재한다면 이미지도 같이 삭제
         if (boardEntity.getThumbFilePath() != null) {
-            File file = new File(boardEntity.getThumbFilePath());
-            if (file.exists()) {
-                file.delete();
-            }
+            s3Uploader.removeObject(boardEntity.getThumbFileName());
         }
-        
+
         boardRepository.delete(boardEntity);
     }
+
+
+    /*게시글 삭제 (로컬) */
+//    @Transactional
+//    public void delete(Long id) {
+//        BoardEntity boardEntity = boardRepository.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다. id=" + id));
+//
+//        // 썸네일 이미지가 존재한다면 이미지도 같이 삭제
+//        if (boardEntity.getThumbFilePath() != null) {
+//            File file = new File(boardEntity.getThumbFilePath());
+//            if (file.exists()) {
+//                file.delete();
+//            }
+//        }
+//
+//        boardRepository.delete(boardEntity);
+//    }
 
     /* 게시글 조회 */
     @Transactional
